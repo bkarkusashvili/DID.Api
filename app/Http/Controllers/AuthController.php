e1985d7b40eddd75fc3b59f40c1bbaee8f9c9604
bkarkusashvili <beqa.qarqusashvili.20@gmail.com>
2022-07-01 19:05:20 +0400

    add auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $token = $request->user()->createToken('web-token');
+
+        dd($token);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'firstname' => 'required|string',
+            'lastname' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        dd($user);
+
+
+        $token = $user->createToken('web-token');
+
+        dd($token);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/test', function () {
-    return response()->json('sa');
-});
+require __DIR__ . '/auth.php';
